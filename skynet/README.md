2f8668e8a57287d76d127da037eed34d50c7e6b2
Onorio Catenacci <catenacci@ieee.org>
2023-09-19 16:34:39 -0400

    My last bits of code for the test assigment as well as a bit of metadata for the reviewer

--- skynet/README.md
+++ skynet/README.md
@@ -1,21 +1,25 @@
 # Skynet
 
-**TODO: Add description**
+Notes:
 
-## Installation
+1.) To start the app:https://stackoverflow.com/users/2820/onorio-catenacci?tab=profile
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `skynet` to your list of dependencies in `mix.exs`:
+{:ok, original_pid} = GenServer.start(Skynet.Terminator, [max_retries: 5])
 
-```elixir
-def deps do
-  [
-    {:skynet, "~> 0.1.0"}
-  ]
-end
-```
+2.) To totally kill all processes:
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/skynet>.
+System.stop(0) (or, of course, one can hit Ctrl+C and a to abort everything.)
+
+This bears with a little commentary.  Since each new terminator process is spun up as a separate process, killing the initial process (e. g. Process.exit(original_pid, :normal) ) won't work here.  It will close down the initial process but it won't do anything to the other terminators that may have been created. Hence System.stop(0) is the right way to close all of it down.
+
+3.) It's quite possible (in fact I've seen it several times) to have the initial "terminator" get destroyed without ever spawning any additional terminators.  I'm not sure if this is intentional or not--I can't quite tell from the specs I was given.
+
+As you'll see I haven't even gotten to the API implmentation.  I could have gone with either a REST API or a GraphQL but I feel like either one of them would only make sense if I were persisting state somewhere--maybe even a SQLite DB.  I hadn't got round to that yet.
+
+I will say that while I agree with asking someone to write Task/GenServer code is surely a good test of Elixir know-how in the jobs I've had professionally coding Elixir it hasn't come up.  I mean--at all.  In all the jobs I've had we wrote code to fetch code from either a GraphQL endpoint or a REST endpoint and we never spawned/tasked/genserver'd up background processes to do so.  I suspect you won't even need to read this paragraph to see how much I struggled to recall this portion of Elixir's library.  It's not an excuse but it is an explanation.  Everything we did we deployed to Docker containers then we deployed the docker containers with Kubernetes on AWS.  But at no point did I need to actually code up GenServers or other mechamisms of that sort--so for this exercise I had to fire up some rarely used neurons. 
+
+So if you've gotten this far and you've looked at my code I suspect you'll decide I'm not a fit for GetThru.  I'm far from finishing the exercise which I'm sure you consider trivial (as I said above, I'd dispute "trivial" given the lack of people using things like GenServer in practice but YMMV). 
+
+If so, then thanks for your time and thanks for helping me to refresh my memory on GenServers etc.  I am honestly more senior and more capable with Elixir than this code probably demonstrates.  Look here: https://stackoverflow.com/users/2820/onorio-catenacci?tab=profile and look at some of the Elixir answers I've provided to people. I'm not bragging or trying to toot my own horn--just trying to insure you're aware of my honest weaknesses and strengths. But if I were in your shoes the fact that I failed to finish this after more than a week would be a red flag and I wouldn't blame you if you just pass. 
+
 
--- skynet/lib/skynet/terminator.ex
+++ skynet/lib/skynet/terminator.ex
@@ -1,30 +1,46 @@
 defmodule Skynet.Terminator do
   use GenServer
   require Logger
-  # alias __MODULE__
 
-  defstruct [:id, :task_pid]
   @time_for_death_check 10_000
   @chance_of_death 25
   @time_to_spawn_new 5_000
   @chance_spawn_new 20
 
-  def generate_random_id() do
+  defstruct [:id, :task_pid]
+
+  defp generate_random_id() do
     :crypto.strong_rand_bytes(6) |> Base.encode64(padding: false)
   end
 
-  defp post_death_check(pid) do
-    Process.send_after(pid, :check_for_death, @time_for_death_check)
+  # send_dc_message to pid (send death check message to specified process id)
+  # Post :check_for_death atom (message) to message queue of specified process after specified timeout
+  defp send_dc_message(pid) do
+    Process.send_after(pid, :destroy_terminator, @time_for_death_check)
   end
 
-  defp post_spawn_check(pid) do
-    Process.send_after(pid, :possibly_spawn_terminator, @time_to_spawn_new)
+  # send_sc_message to pid (send spawn check message to specified process id)
+  # Post :spawn_terminator atom (message) to message queue of specified process after specified timeout
+  defp send_sc_message(pid) do
+    Process.send_after(pid, :spawn_terminator, @time_to_spawn_new)
+  end
+
+  defp should_act_on_message?(threshold) do
+    :rand.uniform(100) <= threshold
+  end
+
+  defp terminator_exists?(id, terminator_list) when is_binary(id) do
+    Enum.member?(terminator_list, %Skynet.Terminator{id: id})
+  end
+
+  defp terminator_exists?(pid, terminator_list) when is_pid(pid) do
+    Enum.member?(terminator_list, %Skynet.Terminator{task_pid: pid})
   end
 
   def init(init_args) do
     max_retries = Keyword.get(init_args, :max_retries, 5)
-    check_for_spawn()
-    check_for_death()
+    send_sc_message(self())
+    send_dc_message(self())
     state = %{terminator_list: [], max_retries: max_retries}
     {:ok, state}
   end
@@ -35,58 +51,58 @@ defmodule Skynet.Terminator do
     %Skynet.Terminator{id: id, task_pid: pid}
   end
 
-  def check_for_spawn() do
-    post_spawn_check(self())
+  def destroy_terminator(id, terminator_list) when is_binary(id) do
+    if terminator_exists?(id, terminator_list) do
+      terminator_to_be_destroyed = %Skynet.Terminator{id: id}
+      List.delete(terminator_list, terminator_to_be_destroyed)
+      Process.exit(terminator_to_be_destroyed.task_pid, :normal)
+    end
   end
 
-  def check_for_death() do
-    post_death_check(self())
+  def destroy_terminator(pid, terminator_list) when is_pid(pid) do
+    if terminator_exists?(pid, terminator_list) do
+      terminator_to_be_destroyed = %Skynet.Terminator{task_pid: pid}
+      List.delete(terminator_list, terminator_to_be_destroyed)
+      Process.exit(terminator_to_be_destroyed.task_pid, :normal)
+    end
   end
 
-  defp random_chance_passed?(threshold) do
-    :rand.uniform(100) <= threshold
-  end
+  def handle_info(:destroy_terminator, state) do
+    Logger.info("Checking for terminator destruction")
 
-  def kill_terminator(terminator_list) do
-    terminator_to_be_killed = self()
-    List.delete(terminator_list, %Skynet.Terminator{task_pid: terminator_to_be_killed})
-    Process.exit(terminator_to_be_killed, :normal)
-  end
-
-  def handle_info(:check_for_death, state) do
-    Logger.info("Checking for termination")
-
-    if random_chance_passed?(@chance_of_death) do
+    if should_act_on_message?(@chance_of_death) do
       Logger.info("Sarah Connor got this terminator")
-      kill_terminator(state.terminator_list)
+      destroy_terminator(self(), state.terminator_list)
+      {:noreply, state}
     else
-      check_for_death()
+      send_dc_message(self())
       # No need to do anything
       {:noreply, state}
     end
   end
 
-  def handle_info(:possibly_spawn_terminator, state) do
+  def handle_info(:spawn_terminator, state) do
     Logger.info("Checking for spawning new terminator")
 
-    if random_chance_passed?(@chance_spawn_new) do
-      Logger.info("state before spawning new terminator: #{inspect(state)}")
+    if should_act_on_message?(@chance_spawn_new) do
       Logger.info("Spawning new terminator")
       terminator = spawn_terminator()
       terminator_list = state.terminator_list
       terminator_list = [terminator] ++ terminator_list
       List.flatten(terminator_list)
 
-      state = Map.replace(state, :terminator_list, terminator_list)
+      #      state = Map.replace(state, :terminator_list, terminator_list)
+      Map.replace(state, :terminator_list, terminator_list)
+    end
 
-      Logger.info("terminator_list post task #{inspect(state.terminator_list)}")
-      Logger.info("state after spawning new terminator #{inspect(state)}")
-      check_for_spawn()
-      {:noreply, state}
-    else
-      # No need to do anything
-      check_for_spawn()
-      {:noreply, state}
+    send_sc_message(self())
+    {:noreply, state}
+  end
+
+  def handle_info(:list_all_terminators, state) do
+    for t <- state.terminator_list do
+      Logger.info("Terminator id: #{t.id}")
+      Logger.info("Terminator task process id: #{t.task_pid}")
     end
   end
 end
